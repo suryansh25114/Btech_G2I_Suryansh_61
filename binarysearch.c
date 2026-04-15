5abd3b3bc36a3b014864ed3cf6d47c4c26cb91b7
suryansh25114 <suryanshgupta6233@gmail.com>
2026-04-16 02:40:45 +0530

    Add files via upload

---
+++ binarysearch.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+    int a[100],num,i,n,beg,end,mid,found=0;
+    printf("Enter the number of elements :");
+    scanf("%d",&n);
+
+    printf("Enter elements :\n");
+    for(i = 0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+
+    printf("Enter the number that has to be searched :");
+    scanf("%d",&num);
+
+    beg = 0,end = n-1;
+    while(beg<=end){
+        mid = (beg + end)/2;
+        if(a[mid]==num){
+            printf("%d is present in the array at position = %d",num,mid);
+            found = 1;
+            break;
+        }
+        else if(a[mid]>num){
+            end = mid - 1;
+        }
+        else{
+            beg = mid + 1;
+        }
+    }
+
+    if(beg>end && found == 0){
+        printf("%d does not exist in the array ",num);
+    }
+    return 0;
+}
